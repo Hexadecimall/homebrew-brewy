bb1e11be7e737b4cb6b9dbc80733e7d40c6006ee
Hexadecimall <Hexadecimall@users.noreply.github.com>
2026-09-13 08:45:10 -0400

    Point formula at Brewy 0.2.0

--- Formula/brewy.rb
+++ Formula/brewy.rb
@@ -2,15 +2,10 @@ class Brewy < Formula
   desc "Fast terminal interface for Homebrew"
   homepage "https://github.com/Hexadecimall/homebrew-brewy"
   url "https://github.com/Hexadecimall/homebrew-brewy.git",
-      tag:      "v0.1.1",
-      revision: "6feb4f8b31e5f02e97a3d263be9a62e518f86bf7"
+      tag:      "v0.2.0",
+      revision: "6395d05ce5bb66659929793493ea16e94da58ea7"
   license "MIT"
   head "https://github.com/Hexadecimall/homebrew-brewy.git", branch: "main"
-
-  bottle do
-    root_url "https://raw.githubusercontent.com/Hexadecimall/homebrew-brewy/main/bottles"
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "04ed4bf68b91fc113870b56e49d890429e64b57392e7185320b4165ab2921dea"
-  end
 
   depends_on "rust" => :build
   depends_on :macos
